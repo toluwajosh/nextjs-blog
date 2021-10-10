33790a0768bcf177e38e45cefb33766524f3b69d
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2021-10-10 14:30:23 +0900

    First commit

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+// https://nextjs.org/learn/basics/assets-metadata-css/styling-tips
+
+// module.exports = {
+//   plugins: [
+//     'tailwindcss',
+//     'postcss-flexbugs-fixes',
+//     [
+//       'postcss-preset-env',
+//       {
+//         autoprefixer: {
+//           flexbox: 'no-2009'
+//         },
+//         stage: 3,
+//         features: {
+//           'custom-properties': false
+//         }
+//       }
+//     ]
+//   ]
+// }
